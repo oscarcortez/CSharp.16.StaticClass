e41a867c18551e2722009ee51603d79de5fcb57e
oscarcortez <oscarkortez@gmail.com>
2021-03-31 19:38:59 -0400

    Add project files.

---
+++ ConsoleApp4StaticClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using Models;
+using System;
+
+namespace ConsoleApp4StaticClass
+{
+    class Program
+    {
+        /// <summary>
+        /// STATIC: una clase estatica no puede ser instanciada
+        /// una clase normal puede tener static methods y se las llama sin instanciar la clase
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Calculadora.suma: {Calculadora.Suma(4,5) }");
+
+            Calculadora c = new Calculadora();
+
+            Console.WriteLine($"calculadora instanciada: {c.Mult(2, 4)}");
+            
+        }
+    }
+}
---
+++ Models/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Models
+{
+    public class Calculadora
+    {
+        public static int Suma(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Mult(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
